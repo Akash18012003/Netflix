430fd4781ddb5911c44c3ee1d3072a847a911f22
Akash18012003 <90127503+Akash18012003@users.noreply.github.com>
2024-01-02 21:56:50 +0530

    Update Card.jsx

--- netflix-ui/src/components/Card.jsx
+++ netflix-ui/src/components/Card.jsx
@@ -27,7 +27,7 @@ export default React.memo(function Card({ index, movieData, isLiked = false }) {
 
   const addToList = async () => {
     try {
-      await axios.post("https://netflix-51gptaog8-akashs-projects-c96b2f8a.vercel.app/api/user/add", {
+      await axios.post("https://netflix-ivory-mu.vercel.app/api/user/add", {
         email,
         data: movieData,
       });
